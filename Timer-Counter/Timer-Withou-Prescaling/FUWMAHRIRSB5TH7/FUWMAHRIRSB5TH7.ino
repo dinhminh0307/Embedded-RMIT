8ea062bd2f0a797a956861ccde227f940a6a1491
minhxiro <94245285+minhxiro@users.noreply.github.com>
2023-08-17 10:41:09 +0700

    Update lecture

---
+++ Timer-Counter/Timer-Withou-Prescaling/FUWMAHRIRSB5TH7/FUWMAHRIRSB5TH7.ino
@@ -0,0 +1,19 @@
+// GPIO as output
+ /*Masking technique, using bit manipulation*/
+#ifndef F_CPU
+#define F_CPU 16000000UL 
+#endif
+#include <avr/io.h> // Thi s header def i nes al l t he I nput / Out put Regi st er s f or al l AVR
+#include <util/delay.h> /// Thi s header f i l e def i nes t wo f unct i ons, _del ay_ms ( mi l l i second
+
+int main(void) {
+  DDRB |= (1 << 5); //Set port B5 as output
+  TCCR1B |= (1 << CS10); // TTCR1B used to set mode for timer, setup timer1 (16 bit) since 
+
+  while (1) {
+    if (TCNT1 >= 7999) { // TCNT1 will hold the value count incrementingly, if it counts up over the target time, toggle the port and reset timer
+      PORTB ^= (1 << 5); // toggle the port B5
+      TCNT1 = 0; // Reset timer coutner
+    }
+  }
+}
