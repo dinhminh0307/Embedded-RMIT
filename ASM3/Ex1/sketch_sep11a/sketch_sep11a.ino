7b93f1c9dd1da323a1a71686329c19b8ceb2c103
minhxiro <94245285+minhxiro@users.noreply.github.com>
2023-09-22 01:12:48 +0700

    Fix ex 1

--- ASM3/Ex1/sketch_sep11a/sketch_sep11a.ino
+++ ASM3/Ex1/sketch_sep11a/sketch_sep11a.ino
@@ -4,47 +4,43 @@
 volatile bool buttonPressed = false;
 volatile bool quarterFlag = false;
 volatile bool halfFlag = false;
-int main(void) {
-  DDRB |= (1 << 5);
+void btn_init() {
   DDRD &= ~(1 << 2);
-  TCCR0B |= ((1 << CS01) | (1 << CS00)); // Enable to the 64 prescaler
-  TCCR0A |= (1 << WGM01); //Set up TCT mode for timer 0
-  
   EICRA |= (1 << ISC01);
   EIMSK |= (1 << INT0); // enbale port D2 as external interupt
+  sei(); //Enable globale interupt bit
+}
+
+void timer0_init() {
+  TCCR0B |= ((1 << CS01) | (1 << CS00)); // Enable to the 64 prescaler
+  TCCR0A |= (1 << WGM01); //Set up TCT mode for timer 0
   TIMSK0 |= (1 << OCIE0A); //ENbale COmparematch A interupt
   OCR0A = 249;
+}
 
-  // For TC1
+void timer1_init() {
+   // For TC1
   TCCR1A |= (1 << WGM12); // Setup CTC mode for timer 1
   TCCR1B |= ((1 << CS10) | (1 << CS11)); // Set prescaler for timer 1
   TIMSK1 |= (1 << OCIE1A); // Enable COmp A interupt
   OCR1A = 24999;
-
-  
-  sei(); //Enable globale interupt bit
-
+}
+int main(void) {
+  DDRB |= (1 << 5);
+  btn_init();
+  timer0_init();
+  timer1_init();
   while(1) {
     while(buttonPressed) {
-      // PORTB |= (1 << 5);
-      // _delay_ms(2000);
-      // PORTB &= ~(1 << 5);
-      // _delay_ms(2000);
-      
       if(quarterFlag) {
         PORTB ^= (1 << 5);
         quarterFlag = false;
     }
   }
-    // PORTB |= (1 << 5);
-    // _delay_ms(1);
-    // PORTB &= ~(1 << 5);
-    // _delay_ms(1);
     if(halfFlag) {
     PORTB ^= (1 << 5);
     halfFlag = false;
     }
-    
   }
 }
 
@@ -55,10 +51,6 @@ ISR(INT0_vect) {
   else {
     buttonPressed = true;
   }
-  //  PORTB |= (1 << 5);
-  //     _delay_ms(2000);
-  //     PORTB &= ~(1 << 5);
-  //     _delay_ms(2000);
 }
 
 ISR(TIMER1_COMPA_vect) {
--- ASM3/Ex4/sketch_sep20a/sketch_sep20a.ino
+++ ASM3/Ex4/sketch_sep20a/sketch_sep20a.ino
@@ -11,14 +11,24 @@
 #define CE 1
 #define DAT 5
 volatile bool buttonPressed = false;
-void CE_Enable();
-void sendCommandByte(uint8_t commandByte);
-void SCLK_Triggered();
-void CE_Disable();
-void sendBit(int tmp);
+// void CE_Enable();
+// void sendCommandByte(uint8_t commandByte);
+// void toggleData(int tmp)
+// // void SCLK_Triggered();
+// void CE_Disable();
+// void readByte();
+// void sendBit(int tmp);
 int main(void) {
+   UBRR0H = ( BAUD_PRESCALE >> 8); // Load upper 8- bits of the baud rate value into the high byte of the UBRR0H register
+   UBRR0L = BAUD_PRESCALE ; // Load lower 8- bits of the baud rate value into the low byte of the UBRR0L register
+   //Configure data format for transimission
+   UCSR0C |= (1 << UCSZ00 ) | (1 << UCSZ01 ); // Use 8- bit character sizes
+   UCSR0B |= (1 << RXEN0 ) | (1 << TXEN0 );
+
   DDRB |= (1 << CE);
   DDRB |= (1 << SCLK);
+  DDRB |= (1 << DAT);
+  DDRB |= (1 << 4);
   DDRD &= ~(1 << 2); //Set port d2 as input
   EICRA |= (1 << ISC01); // Detect the falling edge
   EIMSK |= (1 << INT0); // enable exint in portd2
@@ -26,25 +36,45 @@ int main(void) {
   while(1) {
     if (buttonPressed) {
       sendCommandByte(MIN_READ);
+      buttonPressed = false;
     }
+    
   }
 }
+
+// void readByte() {
+//   while (( UCSR0A & (1 << RXC0 )) == 0) {};
+//   int recievedByte = UDR0;
+//   for(int i = 0; i < 8; i ++) {
+//     int singleBit = (recievedByte & (1 << i));
+//     toggleData(singleBit);
+//   }
+// }
+// void toggleData(int tmp) {
+//   PORTB |= (1 << 4);
+//   PORTB ^= (1 << 4);
+// }
+
 
 void sendCommandByte(uint8_t commandByte) {
   CE_Enable();
   for(int i = 0; i < 8;i++) {
-    int singleBit = (commandByte & (1 << i));
+    int singleBit = (commandByte | (1 << i));
     sendBit(singleBit);
+    SCLK_Triggered();
   }
   CE_Disable();
 }
 void SCLK_Triggered() {
   PORTB |= (1 << SCLK);
+  PORTB |= (1 << 4);
   PORTB ^= (1 << SCLK);
+  PORTB ^= (1 << 4);
 }
 void sendBit(int tmp) {
   if(tmp != 0) {
     PORTB |= (1 << DAT);
+    
   }
   else {
     PORTB &= ~(1 << DAT);
