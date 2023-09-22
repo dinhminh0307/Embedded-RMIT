9cfa440f529d6ae229bf78651d75ac3a998f8aca
minhxiro <94245285+minhxiro@users.noreply.github.com>
2023-09-22 15:13:07 +0700

    Update ex4

--- ASM3/Ex4/sketch_sep20a/sketch_sep20a.ino
+++ ASM3/Ex4/sketch_sep20a/sketch_sep20a.ino
@@ -3,6 +3,7 @@
 #endif
 
 #include <avr/io.h>
+#include <avr/delay.h>
 #define USART_BAUDRATE 9600
 //Take the formula from datasheet 
 #define BAUD_PRESCALE ((( F_CPU / 16) / ( USART_BAUDRATE ))- 1)
@@ -10,76 +11,79 @@
 #define SCLK 3
 #define CE 1
 #define DAT 5
+#define OSC 4
 volatile bool buttonPressed = false;
-// void CE_Enable();
-// void sendCommandByte(uint8_t commandByte);
-// void toggleData(int tmp)
-// // void SCLK_Triggered();
-// void CE_Disable();
-// void readByte();
-// void sendBit(int tmp);
-int main(void) {
-   UBRR0H = ( BAUD_PRESCALE >> 8); // Load upper 8- bits of the baud rate value into the high byte of the UBRR0H register
-   UBRR0L = BAUD_PRESCALE ; // Load lower 8- bits of the baud rate value into the low byte of the UBRR0L register
-   //Configure data format for transimission
-   UCSR0C |= (1 << UCSZ00 ) | (1 << UCSZ01 ); // Use 8- bit character sizes
-   UCSR0B |= (1 << RXEN0 ) | (1 << TXEN0 );
 
-  DDRB |= (1 << CE);
-  DDRB |= (1 << SCLK);
-  DDRB |= (1 << DAT);
-  DDRB |= (1 << 4);
+void btn_init() {
   DDRD &= ~(1 << 2); //Set port d2 as input
   EICRA |= (1 << ISC01); // Detect the falling edge
   EIMSK |= (1 << INT0); // enable exint in portd2
   sei(); // Enalble global interupt
-  while(1) {
-    if (buttonPressed) {
-      sendCommandByte(MIN_READ);
-      buttonPressed = false;
+}
+
+uint8_t readByte() {
+  uint8_t tmp;
+  for(int i = 0; i < 8; i++) {
+    if(PINB & (1 << DAT)) {
+      tmp |= (1 << i);
     }
-    
+    // Falling Edge of the CLK
+    PORTB |= (1 << SCLK);
+    _delay_ms(1);
+    PORTB &= ~(1 << SCLK);
+  }
+  return tmp;
+}
+
+void OSC_ReadData(uint8t_t ctr) {
+  DDRB |= (1 << OSC);
+  for(int i = 0; i < 8; i++) {
+        if((ctr >> i) & 0x01) {
+          PORTB |= (1 << OSC);
+        }
+        else {
+          PORTB &= ~(1 << OSC);
+        }
+      }
+}
+
+uint8_t RTC_ReadResponseByte() {
+  DDRB &= ~(1 << DAT);
+  uint8_t byte = readByte();
+  return byte;
+}
+void writeByte(uint8_t dat) {
+  for(int i = 0; i < 8; i++) {
+    if((dat >> i) & 0x01) {
+      PORTB |= (1 << DAT);
+    }
+    else {
+      PORTB &= ~(1 << DAT);
+    }
+    //TRigger CLK on Rising Edge
+    PORTB &= ~(1 << SCLK);
+    _delay_ms(1);
+    PORTB |= (1 << SCLK);
   }
 }
 
-// void readByte() {
-//   while (( UCSR0A & (1 << RXC0 )) == 0) {};
-//   int recievedByte = UDR0;
-//   for(int i = 0; i < 8; i ++) {
-//     int singleBit = (recievedByte & (1 << i));
-//     toggleData(singleBit);
-//   }
-// }
-// void toggleData(int tmp) {
-//   PORTB |= (1 << 4);
-//   PORTB ^= (1 << 4);
-// }
+void RTC_WriteCommandByte(uint8_t dat) {
+  PORTB &= ~(1 << DAT);
+  writeByte(dat);
+}
 
 
-void sendCommandByte(uint8_t commandByte) {
-  CE_Enable();
-  for(int i = 0; i < 8;i++) {
-    int singleBit = (commandByte | (1 << i));
-    sendBit(singleBit);
-    SCLK_Triggered();
-  }
-  CE_Disable();
+void RTC_init() {
+  DDRB |= (1 << CE);
+  PORTB &= ~(1 << CE);
+
+  DDRB |= (1 << DAT);
+  PORTB &= ~(1 << DAT);
+
+  DDRB |= (1 << SCLK);
+  PORTB &= ~(1 << SCLK);
 }
-void SCLK_Triggered() {
-  PORTB |= (1 << SCLK);
-  PORTB |= (1 << 4);
-  PORTB ^= (1 << SCLK);
-  PORTB ^= (1 << 4);
-}
-void sendBit(int tmp) {
-  if(tmp != 0) {
-    PORTB |= (1 << DAT);
-    
-  }
-  else {
-    PORTB &= ~(1 << DAT);
-  }
-}
+
 void CE_Enable() {
   PORTB |= (1 << CE);
 }
@@ -87,6 +91,29 @@ void CE_Enable() {
 void CE_Disable() {
   PORTB &= ~(1 << CE);
 }
+
+int main(void) {
+   UBRR0H = ( BAUD_PRESCALE >> 8); // Load upper 8- bits of the baud rate value into the high byte of the UBRR0H register
+   UBRR0L = BAUD_PRESCALE ; // Load lower 8- bits of the baud rate value into the low byte of the UBRR0L register
+   //Configure data format for transimission
+   UCSR0C |= (1 << UCSZ00 ) | (1 << UCSZ01 ); // Use 8- bit character sizes
+   UCSR0B |= (1 << RXEN0 ) | (1 << TXEN0 );
+
+  RTC_init();
+  btn_init();
+  while(1) {
+    uint8_t ctr;
+    if (buttonPressed) {
+      CE_Enable();
+      RTC_WriteCommandByte(MIN_READ);
+      ctr = RTC_ReadResponseByte();
+      OSC_ReadData(ctr);
+      CE_Disable();
+    }
+  }
+}
+
+
 ISR(INT0_vect) {
   buttonPressed = true;
 }
