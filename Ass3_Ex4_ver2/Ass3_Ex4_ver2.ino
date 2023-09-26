19cf9ed1ed58c78a800ac3f523ab96eb09531ef4
minhxiro <94245285+minhxiro@users.noreply.github.com>
2023-09-26 22:42:29 +0700

    Push ver 3

--- Ass3_Ex4_ver2/Ass3_Ex4_ver2.ino
+++ Ass3_Ex4_ver2/Ass3_Ex4_ver2.ino
@@ -1,27 +1,29 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/setbaud.h>
+#include <util/delay.h>
 #define CE 0
 #define SCLK 1
 #define DAT 2
 
+
 int cycle = 0;
 volatile int buttonPressed = 0;
+int hour;
 int i = 0;
+int j = 0;
+uint8_t temp = 0x00;
 
-void writeCommand();
-
+void writeCommand(int i);
 
 int main(void)
 {
-  //Setup UART
-  UBRR0H = UBRRH_VALUE;
-  UBRR0L = UBRRL_VALUE;
-  /* Enable UART transmitter/receiver */
-  UCSR0B = (1 << TXEN0) | (1 << RXEN0);
-    /* 8 data bits, 1 stop bit */
-  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+  // Init UART
+  Serial.begin(9600);
 
+    /* Enable UART transmitter/receiver */
+    UCSR0B = (1 << TXEN0) | (1 << RXEN0);
+    /* 8 data bits, 1 stop bit */
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 	DDRB |= (1 << 5);	// Set PORTB5 as output for LED - for TESTING
 	DDRD &= ~(1 << 2); // Set PD2 pin (INT0) as input to receive interrupt re-quest
   DDRB |= (1 << CE) | (1 << SCLK); //PORTB0 - CE & PORTB1 - SCLK as output
@@ -30,9 +32,14 @@ int main(void)
 
   TCCR1B |= (1 << WGM12); //CTC Mode On
   TCCR1B |= (1 << CS11) | (1 << CS10); //Prescaler of 64
-  OCR1A = 62499; //Duration: NOT 0.1s
+  OCR1A = 2499; //Duration: 0.05s
   TIMSK1 |= (1 << OCIE1A); //Output Compare Match A Interrupt On
 	sei(); // Turn on the Global Interrupt Enable Bit
+
+  UCSR0B = (1 << RXEN0 ) | (1 << TXEN0 ); // Turn on the transmission and reception circuitry
+  UCSR0C = (1 << UCSZ00 ) | (1 << UCSZ01 ); // Use 8- bit character sizes
+
+  UCSR0B = (1 << RXCIE0) | (1 << TXCIE0); //Enable RX and TX Complete Interrupt
 
 	while (1)
 	{
@@ -47,34 +54,44 @@ ISR ( TIMER1_COMPA_vect ) {
     if (cycle == 1) { //PORTB0 ON
       PORTB |= (1 << CE);
     } else if (cycle >= 2 && cycle <= 16) { //PORTB1 toggles
-      if (cycle % 2 == 0) { //rising-edge
-        PORTB ^= (1 << SCLK);
+      PORTB ^= (1 << SCLK);
+
+      if (PORTB & (1 << SCLK)) { //rising-edge
         writeCommand(i);
         i++;
-      } else { 
-        PORTB ^= (1 << SCLK);
       }
     } else if (cycle >= 17 && cycle <= 31) {
       PORTB ^= (1 << SCLK);
       DDRB &= ~(1 << DAT); //Input to DS1302 
       //PLUG OSC AND READ THIS PIN: PIND0
+      for(int k = 0; k <=7; k++) {
+        if(readData() == 1) {
+          temp |= (1 << k);
+        }
+        else {
+          temp &= ~(1 << k);
+        }
+      }
+      
     } else if (cycle > 31) { //PORTB1 toggles full 15 cycles
       cycle = 0; //reset cycle to 0 for next button pressed
       buttonPressed = 0; //reset button
 
       PORTB &= ~(1 << CE); //PORTB0 OFF
+      temp = 0x00;
     }
   }
+  Serial.println(temp);
 }
   
 
 ISR ( INT0_vect )	// ISR for the INT0
 { 
   buttonPressed = 1;
+  _delay_ms(1000);
 }
 
 void writeCommand(int i) {
-  while (i <= 7) {
     switch (i) {
       case 0:
         PORTB |= (1 << DAT); //1
@@ -100,11 +117,12 @@ void writeCommand(int i) {
       case 7:
         PORTB |= (1 << DAT); //1
         break;
-    }
+      default:
+        break;
   }
 }
 
-/*
+
 int readData() { //NOT DONE
   for (i = 0; i <= 7; i++) {
     if ( PINB & (1 << DAT) ) { //1
@@ -114,4 +132,4 @@ int readData() { //NOT DONE
     }
   }
 }
-*/
+
